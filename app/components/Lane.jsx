208ec615a871e779b64537523d5a3db7429af3b9
rohmanhakim <rohmanhakim@live.com>
2016-11-19 23:13:31 +0700

    Added features to edit and remove Lane

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -4,24 +4,36 @@ import Notes from './Notes'
 import NoteActions from '../actions/NoteActions'
 import NoteStore from '../stores/NoteStore'
 import LaneActions from '../actions/LaneActions'
+import Editable from './Editable'
 
 class Lane extends React.Component {
   render () {
       const {lane, ...props} = this.props;
       return(
         <div {...props}>
-          <div className="lane-header">
+          <div className="lane-header" onClick={this.activateLaneEdit}>
             <div className="lane-add-note">
               <button onClick={this.addNote}>+</button>
             </div>
-            <div className="lane-name">{lane.name}</div>
+            <Editable
+              className="lane-name"
+              editing={lane.editing}
+              value={lane.name}
+              onEdit={this.editName}/>
+            <div
+              className="lane-delete">
+              <button onClick={this.deleteLane}>x</button>
+            </div>
           </div>
           <AltContainer
             stores={[NoteStore]}
             inject={{
               notes: () => NoteStore.getNotesByIds(lane.notes)
             }}>
-            <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+            <Notes
+              onValueClick={this.activateNoteEdit}
+              onEdit={this.editNote}
+              onDelete={this.deleteNote} />
           </AltContainer>
         </div>
       );
@@ -29,11 +41,16 @@ class Lane extends React.Component {
   editNote(id,task) {
     // Don't modify if trying to set an empty value
     if(!task.trim()) {
+      NoteActions.update({id,editing:false});
       return;
     }
-    NoteActions.update({id,task});
+    NoteActions.update({id,task,editing: false});
   }
   addNote = (e) => {
+    // If note is added, avoid opening lane name edit by stopping
+    // event bubbling in this case.
+    e.stopPropagation();
+
     const laneId = this.props.lane.id;
     const note = NoteActions.create({task: 'New task'});
     LaneActions.attachToLane({
@@ -46,6 +63,26 @@ class Lane extends React.Component {
     const laneId = this.props.lane.id;
     LaneActions.detachFromLane({laneId,noteId});
     NoteActions.delete(noteId);
+  }
+  editName = (name) => {
+    const laneId = this.props.lane.id;
+    // Don't modify if trying to set an empty value
+    if(!name.trim()) {
+      LaneActions.update({id: laneId, editing: false});
+      return;
+    }
+    LaneActions.update({id: laneId,name,editing:false});
+  }
+  deleteLane = () => {
+    const laneId = this.props.lane.id;
+    LaneActions.delete(laneId);
+  }
+  activateLaneEdit = () => {
+    const laneId = this.props.lane.id;
+    LaneActions.update({id:laneId,editing:true});
+  }
+  activateNoteEdit = () => {
+    NoteActions.update({id,editing: true});
   }
 }
 
