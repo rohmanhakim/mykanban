f34e0e32e29c535555d41c1caec4c1fe1f8c610a
rohmanhakim <rohmanhakim@live.com>
2016-11-20 22:18:31 +0700

    Added test for Editable, check if it renders any given value

---
+++ tests/editable_test.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  renderIntoDocument,
+  findRenderedDOMComponentWithClass
+} from 'react-addons-test-utils';
+import assert from 'assert';
+import Editable from '../app/components/Editable'
+
+describe('Editable', () => {
+  it('renders value', () => {
+    const value = 'value';
+    const component = renderIntoDocument(
+      <Editable value={value} />
+    );
+
+    const valueComponent = findRenderedDOMComponentWithClass(component,'value');
+    assert.equal(valueComponent.textContent,value);
+  });
+});
