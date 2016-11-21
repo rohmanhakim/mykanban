a6fd9f0fdd403888b01f271528f6378df67ec04d
rohmanhakim <rohmanhakim@live.com>
2016-11-21 20:05:37 +0700

    Added test for NoteStore. Should delete a note

--- tests/note_store_test.js
+++ tests/note_store_test.js
@@ -24,4 +24,11 @@ describe('NoteStore', () => {
     assert.equal(state.notes.length,1);
     assert.equal(state.notes[0].task,updatedTask);
   });
+  it('deletes notes', () => {
+    NoteActions.create({task: 'test'});
+    const note = NoteStore.getState().notes[0];
+    NoteActions.delete(note.id);
+    const state = NoteStore.getState();
+    assert.equal(state.notes.length, 0);
+  });
 });
