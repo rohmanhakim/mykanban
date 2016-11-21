4105b1e9f52eb0ec0c583a6bae17d72800cc0f53
rohmanhakim <rohmanhakim@live.com>
2016-11-21 20:01:18 +0700

    Added tets for NoteStore. Should update a note

--- tests/note_store_test.js
+++ tests/note_store_test.js
@@ -4,11 +4,24 @@ import NoteStore from 'app/stores/NoteStore'
 import alt from 'app/libs/alt'
 
 describe('NoteStore', () => {
+  beforeEach(() => {
+    alt.flush();
+  });
   it('create notes', () => {
     const task = 'test';
     NoteActions.create({task});
     const state = NoteStore.getState();
     assert.equal(state.notes.length,1);
     assert.equal(state.notes[0].task,task);
+  });
+  it('updates notes',() => {
+    const task = 'test';
+    const updatedTask = 'test 2';
+    NoteActions.create({task});
+    const note = NoteStore.getState().notes[0];
+    NoteActions.update({...note,task: updatedTask});
+    const state = NoteStore.getState();
+    assert.equal(state.notes.length,1);
+    assert.equal(state.notes[0].task,updatedTask);
   });
 });
